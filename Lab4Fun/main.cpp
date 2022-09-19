11028e867dbde95f19a28fe2f038176aac7a4a9a
Maxim Voyevoda <maximvoyevoda@gmail.com>
2022-09-18 22:51:49 -0400

    Initial Commit

---
+++ Lab4Fun/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab4Fun
+//
+//  Created by Maxim Voyevoda on 9/18/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
